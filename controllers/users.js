cb165189746a3cc7f49a2d8af5846d20e323009a
Arvind <ari0997@gmail.com>
2019-02-20 07:57:32 +0530

    Added User Model and created signup methods

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,16 @@ module.exports = function (_) {
     return {
         SetRouting: function (router) {
             router.get('/', this.indexPage);
+            router.get('/signup', this.getSignup);
         },
-        
+
         indexPage: function (req, res) {
-            return res.render('index', {test: 'This is a test'});
+            console.log("IndexRouter");
+            return res.render('index');
+        },
+
+        getSignup: function (req, res) {
+            return res.render('signup');
         }
     }
 };
--- server.js
+++ server.js
@@ -2,16 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+// const flash = require('connect-flash');
+const passport = require('passport');
+
 const container = require('./container');
 
 container.resolve(function (users) {
 
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/privatechat', {useNewUrlParser: true});
     const app = setupExpress();
 
     function setupExpress() {
         const app = express();
         const server = http.createServer(app);
-        const PORT = 3000;
+        const PORT = process.env.PORT || 3000;
         server.listen(PORT, function () {
             console.log(`Listening on port ${PORT}`)
         });
@@ -25,9 +35,24 @@ container.resolve(function (users) {
 
 
     function ConfigureExpress(app) {
+        require('./passport/passport-local');
         app.use(express.static('public'));
+        app.use(cookieParser());
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+
+        app.use(validator());
+        app.use(session({
+            secret: 'thisisasecretkey',
+            resave: true,
+            saveInitialized: true,
+            saveUninitialized: false,
+            store: new MongoStore({mongooseConnection: mongoose.connection})
+        }));
+        //
+        // app.use(flash);
+        app.use(passport.initialize());
+        app.use(passport.session());
     }
 });
